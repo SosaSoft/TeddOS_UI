4a071cf3dd26ef10e3407b0f43641acab6776eb9
Carlos Sosa <carlos.sosaa@hotmail.com>
2017-09-24 18:13:42 -0600

    Principal system main structure

--- main.c
+++ main.c
@@ -1,30 +1,30 @@
 /* 
- * File:   main.c
- * Author: <Ary Hernandez>
+ * File:        main.c
+ * Author:      <Ary Hernandez>
+ * Modifier:    <Carlos Sosa>
  *
  * Created:     <--/--/2017>
- * Modified:    <--/--/---->
- */
+ * Modified:    <24/09/2017>
+ *//
 
 /* Librerias a utilizar */
 #include <stdio.h>
 
-/* Headers del sistema */
+/* Header del sistema */
 #include "system.h"
-#include "calc.h"
 
 /**
 * Main Loop
 */
 int main (void) {
-    char *str = "Hola Mundo \n\n Somos los Debian: \n\t -Ary Hernandez \n\t -Marc Guzman \n\t -Carlos Sosa \n\t -Jose Araujo \n\t -Andres Alvarez", *ch;
-	unsigned short *vidmem = (unsigned short*) 0xb8000;
-	unsigned i;
-	for (ch = str, i = 0; *ch; ch++, i++) {
-        vidmem[i] = (unsigned char) *ch | 0x0700;
-    } 
+    // System init
+    // sys_init();
+
+    // System header
+    // sys_header();
     
-    /* CALL SHELL LOOP */
+    // Shell loop
+    shLoop();
 
 	return 0;
 }
